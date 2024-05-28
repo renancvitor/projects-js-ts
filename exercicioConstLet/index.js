2d5d21b4923ac0355ceb3a49b6a136b20a43f4ac
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-28 19:27:35 -0300

    Exercício com const e let

--- exercicioConstLet/index.js
+++ exercicioConstLet/index.js
@@ -11,5 +11,5 @@ let imc = peso / (altura * altura);
 let anoNascimento = 2024 - idade;
 
 console.log(nome, sobrenome, 'tem', idade, 'anos, pesa', peso + 'kg');
-console.log('tem ', altura, 'de altura e seu IMC é de ', imc);
+console.log(`tem ${altura} de altura e seu IMC é de ${imc}`);
 console.log(nome, 'nasceu em ', anoNascimento + '.');
