0fea5c07fab04ec05ffe130ebc08fb4e5dbb7fd7
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-05-30 07:35:56 -0300

    Exercício com alert, confirm e prompt

---
+++ alertConfirmPrompt/js/script.js
@@ -0,0 +1,8 @@
+let num1 = prompt('Digite um número:'); //137
+let num2 = prompt('Digite outro número:'); //233
+typeof num1;
+typeof num2;
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+alert(`O resultado da operação é: ${resultado}`); //370
