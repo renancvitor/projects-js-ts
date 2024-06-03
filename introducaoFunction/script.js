da6c75dd439cd8cef7926b073aef8fe3d94967e8
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-06-03 18:12:05 -0300

    Introdução à Function no JavaScript

---
+++ introducaoFunction/script.js
@@ -0,0 +1,24 @@
+const raiz = n => n ** 0.5;
+
+// const raiz = (n) => {
+//     return n ** 0.5;
+// };
+
+// const raiz = function (n) {
+//     return n ** 0.5;
+// };
+
+console.log(raiz(9));
+console.log(raiz(81));
+console.log(raiz(16));
+console.log(raiz(25));
+
+// function soma(x, y) {
+//     const resultado = x + y;
+//     return resultado;
+// }
+
+// console.log(soma('Luiz', ' Otávio'));
+// console.log(soma(2, 2));
+// console.log(soma(3, 6));
+// console.log(soma(5, 10));
