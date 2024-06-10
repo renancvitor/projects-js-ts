902d82a4c1092122cdf9474f50d933ee155c84b4
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-06-10 20:32:40 -0300

    Primeiro exercício de teste de Lógica de Programação

---
+++ exerciciosLogicaProgramacao/main.js
@@ -0,0 +1,14 @@
+function max(x, y) {
+    // if (x > y) {
+    //     return x;
+    // }
+    // return y;
+
+    return x > y ? x : y;
+}
+
+const max2 = (x, y) => {
+    return x > y ? x : y;
+};
+
+console.log(max2(10,20));
