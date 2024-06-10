de4dc194565927e4094342a253873152ac519428
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-06-10 07:06:19 -0300

    Introdução ao for in

---
+++ for-in/main.js
@@ -0,0 +1,25 @@
+const pessoa = { 
+    nome: 'Renan',
+    sobrenome: 'Vitor',
+    idade: 30
+}
+
+for (let i in pessoa) {
+    console.log(`${i}: ${pessoa[i]}`);
+}
+
+console.log(pessoa.nome); // primeira forma de acessar o nome num array
+console.log(pessoa['nome']); // segunda forma de acessar o nome num array
+
+
+
+// const frutas = ['Pera', 'Maçã', 'Uva'];
+
+// for (let i = 0; i < frutas.length; i ++) {
+//     console.log(frutas[i]);
+// }
+
+// for (let i in frutas) {
+//     // console.log(i);
+//     console.log(frutas[i]);
+// }
