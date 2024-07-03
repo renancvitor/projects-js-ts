ebaee23dbccfca8b79161bbbacd98973a6a9bb99
Renan Vitor <renan.vitor.cm@gmail.com>
2024-07-03 20:08:29 -0300

    XMLHttpRequest utilizando Promise() - comentando fetch

--- xml-http-request/assets/js/main.js
+++ xml-http-request/assets/js/main.js
@@ -48,3 +48,12 @@ function carregaResultado(response) {
     const resultado = document.querySelector('.resultado');
     resultado.innerHTML = response;
 }
+
+
+// fetch('pagina1.html')
+//     .then(resposta => {
+//         if (resposta.status !== 200) throw new Error('Erro 404 - nosso');
+//         return resposta.text();
+//     })
+//     .then(html => console.log(html))
+//     .catch( e => console.error(e));
