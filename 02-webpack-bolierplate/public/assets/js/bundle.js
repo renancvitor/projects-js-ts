4f927daf884e057c2e4a4bdef231073fc01cacd2
Renan Vitor <renan.vitor.cm@gmail.com>
2024-07-04 07:13:05 -0300

    Criação e configuração webpack - Modelo principal próximas aulas

---
+++ 02-webpack-bolierplate/public/assets/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// alert('Hello world');
+
+console.log('EIIIIIII');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ 02-webpack-bolierplate/webpack.config.js
@@ -0,0 +1,30 @@
+// Para criar o arquivo package.json, digite o comando npm init -y no terminal do vs code
+// LEMBRANDO que é importante navegar através da pasta do projeto utilizando cd NOMEPASTAPROJETO
+// Caso contrário o package.json será criado na pasta raíz
+
+// Instalando as extensões do babel js e webpack:
+// Digite no terminal: npm install --save-dev @babel/cli @babel/preset-env @babel/core babel-loader webpack webpack-cli regenerator-runtime core-js@2
+
+const path = require('path'); // CommonJS - padrão do Node.Js
+
+module.exports = {
+    mode: 'production', // É possível alternar entre modo development e production
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'assets', 'js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/env']
+                }
+            }
+        }]
+    },
+    devtool: 'source-map'
+};
