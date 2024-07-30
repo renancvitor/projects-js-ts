19b333f9f0f957beb5916e311eff079a1074f91b
Renan C Vitor <renan.vitor.cm@gmail.com>
2024-07-29 21:39:34 -0300

    Express - Middlewares

---
+++ express+middlewares/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
+
+// params === /profiles/id123987
+// query === /?chave1=valor1&chave2=valor2
+// body === <- deve ser tratado no POST ou PUT e deve ter a declaração app.use com extend:true
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(middlewareGlobal);
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
