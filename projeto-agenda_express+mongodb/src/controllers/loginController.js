39b033655ddb8c465786bc647546a0fdfa887e44
renancvitor <renan.vitor.cm@gmail.com>
2024-08-16 08:07:18 -0300

    Projeto Agenda - express + mongodb - Login / Criar conta - Tela login usuário

--- projeto-agenda_express+mongodb/src/controllers/loginController.js
+++ projeto-agenda_express+mongodb/src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/LoginModel')
 
 exports.index = (req, res) => {
-    res.render('login');
+    if(req.session.user) return res.render('login-logado');
+    return res.render('login');
 };
 
 exports.register = async function(req, res) {
@@ -12,16 +13,44 @@ exports.register = async function(req, res) {
         if (login.errors.length > 0) {
             req.flash('errors', login.errors);
             return req.session.save(() => {
-                res.redirect('/login/index'); // Resposta enviada
+                return res.redirect('/login/index');
             });
         }
 
         req.flash('success', 'Seu usuário foi criado com sucesso!');
         return req.session.save(() => {
-            res.redirect('/login/index'); // Resposta enviada
+            return res.redirect('/login/index');
         });
     } catch (e) {
-        console.log(e);
-        return res.render('404'); // Resposta enviada
+        console.error(e);
+        return res.render('404');
     }
+};
+
+exports.login = async function(req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+    
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            return req.session.save(() => {
+                return res.redirect('/login/index');
+            });
+        }
+
+        req.flash('success', 'Login feito com sucesso.');
+        req.session.user = login.user;
+        return req.session.save(() => {
+            return res.redirect('/login/index');
+        });
+    } catch (e) {
+        console.error(e);
+        return res.render('404');
+    }
+};
+
+exports.logout = function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
 };
