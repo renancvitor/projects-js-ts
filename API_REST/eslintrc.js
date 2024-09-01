cd633f738926c23dc7505e2e85456fb0e155a317
renancvitor <renan.vitor.cm@gmail.com>
2024-09-01 10:31:47 -0300

    Estruturando API_REST

---
+++ API_REST/eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ['airbnb-base'],
+  env: {
+      browser: true,
+      node: true,
+      es2021: true,
+  },
+  parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+  },
+  rules: {
+      "no-console": "off",
+      "class-methods-use-this": "off"
+  },
+};
