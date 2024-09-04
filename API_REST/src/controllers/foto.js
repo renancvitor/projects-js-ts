723915e4fa8ff5a0cf4b71c924abd7494a919dba
renancvitor <renan.vitor.cm@gmail.com>
2024-09-04 20:07:34 -0300

    Tratando arquivos enviados - API_REST

--- API_REST/src/controllers/foto.js
+++ API_REST/src/controllers/foto.js
@@ -1,7 +1,19 @@
+import multer from 'multer';
+import multer from '../config/multer';
+
+const upload = multer(multer).single('arquivo');
 
 class FotoController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => {
+      if(err) {
+        return res.status(400).json({
+          error: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    })
   }
 }
 
--- API_REST/src/routes/foto.js
+++ API_REST/src/routes/foto.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import fotoController from '../controllers/foto';
-import multer from '../config/multer';
-
-const upload = multer(multer);
 
 const router = new Router();
 
-router.post('/',upload.single('arquivo'), fotoController.store);
+router.post('/', fotoController.store);
 
 export default router;
