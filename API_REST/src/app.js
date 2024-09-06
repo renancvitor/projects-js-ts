b274b9348d812f41bf0bb1572d92bbc3de3d9aea
renancvitor <renan.vitor.cm@gmail.com>
2024-09-05 21:28:19 -0300

    Adicionado cors - API_REST

--- API_REST/src/app.js
+++ API_REST/src/app.js
@@ -6,12 +6,28 @@ dotenv.config();
 import './database';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 import home from './routes/home';
 import user from './routes/user';
 import token from './routes/token';
 import aluno from './routes/aluno';
 import foto from './routes/foto';
+
+const whitelist = [
+  "http://localhost:3000"
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if(whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 
 class App {
   constructor() {
@@ -21,6 +37,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
