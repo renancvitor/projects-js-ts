5556d97abbb2f28247dc7c69924bec7a9ca63e43
renancvitor <renan.vitor.cm@gmail.com>
2024-09-09 20:15:19 -0300

    Menu de aplicação - base-react

--- base-react/src/App.js
+++ base-react/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Login from './pages/login';
 import GlobalStyle from "./styles/GlobalStyles";
+import Header from "./components/header";
 
 function App() {
   return (
     <>
+    <Header />
     <Login />
     <GlobalStyle />
     </>
