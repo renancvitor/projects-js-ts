f3c4bf91199a76b20113b5e317d051315e2cd3a1
renancvitor <renan.vitor.cm@gmail.com>
2024-09-09 20:42:09 -0300

    Router e History - base-react

--- base-react/src/pages/Page404/index.js
+++ base-react/src/pages/Page404/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Container } from "../../styles/GlobalStyles";
 
 export default function Page404() {
-  return (
+    return (
     <Container>
       <h1>Página de teste.</h1>
     </Container>
