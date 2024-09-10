7d0ddb9f59d61d583c4399dd93daf48f9b6733eb
renancvitor <renan.vitor.cm@gmail.com>
2024-09-09 21:36:50 -0300

    MyRoute -> Rota personalizada e privada - base-react

--- base-react/src/routes/index.js
+++ base-react/src/routes/index.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 
+import MyRoute from "./MyRoute";
 import Login from '../pages/login';
 import Page404 from '../pages/Page404';
 
 export default function AppRoutes() {
   return (
-      <Routes>
-        <Route exact path="/" Component={Login} />
-        <Route path="*" Component={Page404} />
-      </Routes>
-  )
+    <Routes>
+      <MyRoute path="/" element={<Login />} />
+      <MyRoute path="*" element={<Page404 />} />
+    </Routes>
+  );
 }
