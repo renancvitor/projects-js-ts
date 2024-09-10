ec6fe9b3b58730242313ed115a8ed31bce8fb42a
renancvitor <renan.vitor.cm@gmail.com>
2024-09-10 07:21:52 -0300

    React Toastify - base-react

--- base-react/src/App.js
+++ base-react/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import GlobalStyle from "./styles/GlobalStyles";
 import Header from "./components/header";
 import AppRoutes from "./routes";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <AppRoutes />
       <GlobalStyle />
+      <ToastContainer autoClose={3000} className="toast-container" />
     </>
   );
 }
