da39896b347a7f17f280600b6dc1361f4627d0df
renancvitor <renan.vitor.cm@gmail.com>
2024-09-14 07:51:54 -0300

    Criando as primeiras rotas com BrowserRouter, Routes e Route - projeto-vitejs-typescript-reactrouterdom

--- projeto-vitejs-typescript-reactrouterdom/src/main.tsx
+++ projeto-vitejs-typescript-reactrouterdom/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/global.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { Home } from './components/Home';
+import { About } from './components/About';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
