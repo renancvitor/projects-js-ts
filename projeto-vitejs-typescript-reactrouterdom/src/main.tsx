cc80aaae7909a27c5d247f5f6ac3846179bc865c
renancvitor <renan.vitor.cm@gmail.com>
2024-09-15 07:28:14 -0300

    navigate e useNavigate: Redirecionando páginas - projeto-vitejs-typescript-reactrouterdom

--- projeto-vitejs-typescript-reactrouterdom/src/main.tsx
+++ projeto-vitejs-typescript-reactrouterdom/src/main.tsx
@@ -7,6 +7,7 @@ import { Home } from './components/Home';
 import { About } from './components/About';
 import { Menu } from './components/Menu';
 import { Post } from './components/Post';
+import { Redirect } from './components/Redirect';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/about" element={<About />} />
         <Route path="/posts/:id" element={<Post />} />
         <Route path="/posts" element={<Post />} />
+        <Route path="/redirect" element={<Redirect />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
