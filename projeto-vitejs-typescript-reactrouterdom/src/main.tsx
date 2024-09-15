8ea9af8bb8a41ff3256877c0b972b65d3bffc13d
renancvitor <renan.vitor.cm@gmail.com>
2024-09-15 07:07:48 -0300

    Usando Link versus usando a - projeto-vitejs-typescript-reactrouterdom

--- projeto-vitejs-typescript-reactrouterdom/src/main.tsx
+++ projeto-vitejs-typescript-reactrouterdom/src/main.tsx
@@ -5,10 +5,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Home } from './components/Home';
 import { About } from './components/About';
+import { Menu } from './components/Menu';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+    <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
