3f4958129bf575263feb83d1d053c93298ad4946
renancvitor <renan.vitor.cm@gmail.com>
2024-09-19 13:09:14 -0300

    Generics - Tipos basicos

---
+++ introducaoTypescript/src/generics/generics.ts
@@ -0,0 +1,28 @@
+type FilterCallback<U> = (
+  value: U,
+  index?: number,
+  array?: U[],
+) => boolean;
+
+export function meuFilter<T>(
+  array: T[],
+  callbackfn: FilterCallback<T>,
+): T[] {
+  const newArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (callbackfn(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+
+  return newArray;
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const arrayFiltradoOriginal = array.filter((value) => value < 5);
+console.log(arrayFiltradoOriginal);
+
+const arrayFiltrado = meuFilter(array, (value) => value < 5);
+console.log(arrayFiltrado);
