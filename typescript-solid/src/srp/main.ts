f716af23f0e411a5342a707920be743e6da240e4
renancvitor <renan.vitor.cm@gmail.com>
2024-09-21 15:22:02 -0300

    SRP (Single Responsibility Principle - Typescript S.O.L.I.D.

---
+++ typescript-solid/src/srp/main.ts
@@ -0,0 +1,20 @@
+import { ShoppingCart } from "./entities/shopping-cart";
+import { Order } from "./entities/order";
+import { Messaging } from "./services/messaging";
+import { Persistency } from "./services/persistency";
+import { Product } from "./entities/products";
+
+const shoppingCart = new ShoppingCart();
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Lapis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
