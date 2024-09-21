ac04f571c719631c1900e597659bdd56dab640c8
renancvitor <renan.vitor.cm@gmail.com>
2024-09-21 18:38:46 -0300

    ISP (Interface Segregation Principle) - Typescript S.O.L.I.D.

---
+++ typescript-solid/src/isp/main.ts
@@ -0,0 +1,24 @@
+import { ShoppingCart } from "./classes/shopping-cart";
+import { Order } from "./classes/order";
+import { Messaging } from "./services/messaging";
+import { Persistency } from "./services/persistency";
+import { Product } from "./classes/products";
+import { FiftyPercentDiscount, NoDiscount, TenPercentDiscount } from "./classes/discount";
+
+const fiftyPercentDiscount = new FiftyPercentDiscount();
+const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9));
+shoppingCart.addItem(new Product('Caderno', 9.9));
+shoppingCart.addItem(new Product('Lapis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
