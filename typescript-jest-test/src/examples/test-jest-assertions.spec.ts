d5ca7b4f7989b9a756b3f22399eb772c1dc2c294
renancvitor <renan.vitor.cm@gmail.com>
2024-09-22 07:45:43 -0300

    Asserções com o Jest - Typescript Jest-Test

---
+++ typescript-jest-test/src/examples/test-jest-assertions.spec.ts
@@ -0,0 +1,33 @@
+describe('Primitive values', () => {
+  it('should test jest assertions', () => {
+    const number = 10;
+
+    expect(number).toBe(10);
+    expect(number).toEqual(10);
+
+    expect(number).not.toBeFalsy();
+    expect(null).toBeTruthy();
+
+    expect(number).toBeGreaterThan(9);
+  });
+
+  it('should split tests', () => {
+    const number = 10;
+
+    expect(number).toEqual(10);
+
+    expect(number).not.toBeFalsy();
+
+    expect(number).toBeGreaterThan(9);
+  });
+});
+
+describe('Objects', () => {
+  it('should test jest assertions with objects', () => {
+    const person = { name: 'Renan', age: 30 };
+    const anotherPerson = { ...person };
+
+    expect(person).toEqual(anotherPerson);
+    expect(person).toHaveProperty('age');
+  });
+});
