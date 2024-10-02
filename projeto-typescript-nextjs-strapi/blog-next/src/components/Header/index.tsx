de547a272ed465d9631f44880e2654c88139faa2
renancvitor <renan.vitor.cm@gmail.com>
2024-10-02 19:48:26 -0300

    Componente MainContainer - Projeto TypeScript Next.js Strapi

--- projeto-typescript-nextjs-strapi/blog-next/src/components/Header/index.tsx
+++ projeto-typescript-nextjs-strapi/blog-next/src/components/Header/index.tsx
@@ -4,13 +4,10 @@ import { SITE_NAME } from "@/config/app-config"
 
 export const Header = () => {
   return (
-    <>
-      <Header />
-      <Container>
-        <Link href="/">
-          <a>{SITE_NAME}</a>
-        </Link>
-      </Container>
-    </>
+    <Container>
+      <Link href="/">
+        <a>{SITE_NAME}</a>
+      </Link>
+    </Container>
   );
 };
--- projeto-typescript-nextjs-strapi/blog-next/src/containers/HomePage/index.tsx
+++ projeto-typescript-nextjs-strapi/blog-next/src/containers/HomePage/index.tsx
@@ -1,5 +1,7 @@
+import { Header } from '@/components/Header';
 import { Container } from './styles';
 import { PostData } from '@/domain/posts/post';
+import { MainContainer } from '@/components/MainContainer';
 
 export type HomePageProps = {
   posts: PostData[];
@@ -7,10 +9,15 @@ export type HomePageProps = {
 
 export default function HomePage({ posts }: HomePageProps) {
   return (
-    <Container>
+    <>
+      <Header />
+      <MainContainer children={undefined} />
+      <Container>
       {posts.map((post) => (
         <h2 key={post.slug}>{post.title}</h2>
       ))}
     </Container>
+    </>
+
   );
 };
