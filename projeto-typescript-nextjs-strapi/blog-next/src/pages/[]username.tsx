df24dd90355d97f246db4fc9ddd42ae9f66d036a
renancvitor <renan.vitor.cm@gmail.com>
2024-10-02 18:20:16 -0300

    NextJS SSG com getStaticProps - Projeto TypeScript Next.js Strapi

--- projeto-typescript-nextjs-strapi/blog-next/src/pages/[]username.tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-const Username = () => {
-  const router = useRouter();
-  return <h1>Username: {router.query.username}</h1>;
-};
-
-export default Username;
