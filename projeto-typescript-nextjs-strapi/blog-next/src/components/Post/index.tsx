f6bb34f89b8286fc3849e3e97c1cac68e0460036
renancvitor <renan.vitor.cm@gmail.com>
2024-10-03 07:07:58 -0300

    Container PostCover - Projeto TypeScript Next.js Strapi

--- projeto-typescript-nextjs-strapi/blog-next/src/components/Post/index.tsx
+++ projeto-typescript-nextjs-strapi/blog-next/src/components/Post/index.tsx
@@ -3,6 +3,7 @@ import { Header } from "@/components/Header";
 import { MainContainer } from "@/components/MainContainer";
 import { PostData } from "@/domain/posts/post"
 import { Heading } from "../Heading";
+import { PostCover } from "../PostCover";
 
 export type PostProps = {
   post: PostData;
@@ -15,6 +16,7 @@ export const Post = ({ post }: PostProps) => {
 
       <MainContainer>
         <Heading>{post.title}</Heading>
+        <PostCover coverUrl={post.cover.formats.large.url} alt={post.title} />
         <div dangerouslySetInnerHTML={{ __html: post.content }} />
       </MainContainer>
       <Footer />
