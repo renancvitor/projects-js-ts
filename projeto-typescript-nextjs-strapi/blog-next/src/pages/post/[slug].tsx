0d198ddb047ad39aaa69f7179843ca3488e1c466
renancvitor <renan.vitor.cm@gmail.com>
2024-10-07 20:41:51 -0300

    Paginação - parte 05 - Projeto TypeScript Next.js Strapi

--- projeto-typescript-nextjs-strapi/blog-next/src/pages/post/[slug].tsx
+++ projeto-typescript-nextjs-strapi/blog-next/src/pages/post/[slug].tsx
@@ -3,13 +3,25 @@ import { countAllPosts } from "@/data/posts/count-all-posts";
 import { getAllPosts } from "@/data/posts/get-all-posts";
 import { getPosts } from "@/data/posts/getl-posts";
 import { PostData } from "@/domain/posts/post";
+import { useRouter } from "next/router";
 import { GetStaticPaths, GetStaticProps } from "next/types";
+import Error from 'next/error';
 
 export type DynamicPostProps = {
   post: PostData
-}
+};
 
 const DynamicPost = ({ post }: DynamicPostProps) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Página ainda carregando, por favor aguarde...</div>;
+  }
+
+  if (!post?.title) {
+    return <Error statusCode={404} />;
+  }
+
   return <Post post={post} />
 };
 
@@ -33,10 +45,18 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const posts = await getPosts(ctx.params.slug);
+  const slug = ctx.params?.slug;
+  if (!slug) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const posts = await getPosts(slug);
+  const post = posts.length > 0 ? posts[0] : {};
 
   return {
-    props: { post: posts[0] },
-    // revalidate: 5,
+    props: { post: post },
+    revalidate: 600,
   };
 };
